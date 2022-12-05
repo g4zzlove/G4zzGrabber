564569e02bc140088c4b677bdf77eb450ba37265
g4zz <115455659+g4zzlove@users.noreply.github.com>
2022-12-05 06:10:19 -0300

    Delete build.bat

--- build.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-@MODE 101,50
-
-ECHO REMOVING INTERFERENCES && PY -3.10 -m pip uninstall -r interferences.txt
-ECHO INSTALLING REQUIREMENTS && PY -3.10 -m pip install --force-reinstall -r requirements.txt
-CLS && PY -3.10 builder.py
-PAUSE && COLOR 07 && EXIT
